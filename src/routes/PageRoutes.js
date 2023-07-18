d9ae8f510bac7c4ccff7deb9a730f2f105752c3e
Shir Markowitz <shirm91799@gmail.com>
2023-07-18 04:16:40 -0400

    changed route names and folder names

--- src/routes/PageRoutes.js
+++ src/routes/PageRoutes.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import AboutMe from "../temp_pages/About-Me/AboutMe";
-import Portfolio from "../temp_pages/Portfolio/Portfolio";
-import Skills from "../temp_pages/Skills/Skills";
-import Contact from "../temp_pages/Contact/Contact";
+import AboutMe from "../pages/About-Me/AboutMe";
+import Portfolio from "../pages/Portfolio/Portfolio";
+import Skills from "../pages/Skills/Skills";
+import Contact from "../pages/Contact/Contact";
 
 const PageRoutes = () => {
   return (
