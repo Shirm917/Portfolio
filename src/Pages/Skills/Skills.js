bbeffa6b17a6c7b65c5a5e992727a34b0bcf0629
Shir Markowitz <shirm91799@gmail.com>
2023-07-17 20:29:49 -0400

    added hover to skills icons

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -1,19 +1,51 @@
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
+import Tooltip from "@mui/material/Tooltip";
 
 const Skills = () => {
-    return (
-        <Box className="icons">
-            <img src="/assets/icons/html5.svg" alt="html5 icon"/>
-            <img src="/assets/icons/css3.svg" alt="css3 icon"/>
-            <img src="/assets/icons/bootstrap.svg" alt="bootstrap icon"/>
-            <img src="/assets/icons/javascript.svg" alt="javascript icon"/>
-            <img src="/assets/icons/nodedotjs.svg" alt="node.js icon"/>
-            <img src="/assets/icons/express.svg" alt="express icon"/>
-            <img src="/assets/icons/postgresql.svg" alt="postgresql icon"/>
-            <img src="/assets/icons/react.svg" alt="react icon"/>
-            <img src="/assets/icons/redux.svg" alt="redux icon"/>
-        </Box>
-    )
+  const skills = [
+    { text: "HTML5", iconPath: "/assets/icons/html5.svg", alt: "html5 icon" },
+    { text: "CSS3", iconPath: "/assets/icons/css3.svg", alt: "css3 icon" },
+    {
+      text: "MYSQL",
+      iconPath: "/assets/icons/mysql.svg",
+      alt: "mysql icon",
+    },
+    {
+      text: "JAVASCRIPT",
+      iconPath: "/assets/icons/javascript.svg",
+      alt: "javascript icon",
+    },
+    {
+      text: "NODE.JS",
+      iconPath: "/assets/icons/nodedotjs.svg",
+      alt: "node.js icon",
+    },
+    {
+      text: "EXPRESS.JS",
+      iconPath: "/assets/icons/express.svg",
+      alt: "express icon",
+    },
+    {
+      text: "POSTGRESQL",
+      iconPath: "/assets/icons/postgresql.svg",
+      alt: "postgresql icon",
+    },
+    {
+      text: "REACT AND REACT NATIVE",
+      iconPath: "/assets/icons/react.svg",
+      alt: "react/react native icon",
+    },
+    { text: "REDUX", iconPath: "/assets/icons/redux.svg", alt: "redux icon" },
+  ];
+  return (
+    <Box className="icons">
+      {skills.map((skill) => (
+        <Tooltip title={skill.text}>
+          <img src={skill.iconPath} alt={skill.alt} />
+        </Tooltip>
+      ))}
+    </Box>
+  );
 };
 
 export default Skills;
