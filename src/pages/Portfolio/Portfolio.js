87348a822825018171274ecbb2310f9e3c7b57d9
Shir Markowitz <shirm91799@gmail.com>
2024-01-05 00:05:56 -0500

    adding the conditional rendering logic to Portfolio component and returning CarouselLayout component

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,15 +1,8 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import CarouselTabs from "./CarouselTabs";
-import PortfolioCarousel from "./PortfolioCarousel";
-import {
-  chatAppMedia,
-  weatherAppMedia,
-  memoryGameMedia,
-  recipeFinderMedia,
-  currencyConverterMedia,
-} from "./data/carouselData";
-import Buttons from "./Buttons";
+import { carouselLayoutData } from "./data/carouselLayoutData";
+import CarouselLayout from "./CarouselLayout";
 
 const Portfolio = () => {
   const [tabIndex, setTabIndex] = useState(0);
@@ -21,50 +14,14 @@ const Portfolio = () => {
     <Box className="portfolio">
       <CarouselTabs tabIndex={tabIndex} handleTabChange={handleTabChange} />
       <Box sx={{ padding: 2 }}>
-        {tabIndex === 0 && (
-          <Box>
-            <PortfolioCarousel media={chatAppMedia} />
-            <Buttons
-              github="https://github.com/Shirm917/Final_Project"
-              demo="https://final-project-yfoh.onrender.com"
-            />
-          </Box>
-        )}
-        {tabIndex === 1 && (
-          <Box>
-            <PortfolioCarousel media={weatherAppMedia} />
-            <Buttons
-              github="https://github.com/Shirm917/Weather-App"
-              demo="https://weather-app-kohl-eta.vercel.app"
-            />
-          </Box>
-        )}
-        {tabIndex === 2 && (
-          <Box>
-            <PortfolioCarousel media={recipeFinderMedia} />
-            <Buttons
-              github="https://github.com/Shirm917/Recipe_Finder"
-              demo="https://recipe-finder-kappa-two.vercel.app"
-            />
-          </Box>
-        )}
-        {tabIndex === 3 && (
-          <Box>
-            <PortfolioCarousel media={memoryGameMedia} />
-            <Buttons
-              github="https://github.com/Shirm917/Hackathon1"
-              demo="https://memory-game-delta-vo.vercel.app"
-            />
-          </Box>
-        )}
-        {tabIndex === 4 && (
-          <Box>
-            <PortfolioCarousel media={currencyConverterMedia} />
-            <Buttons
-              github="https://github.com/Shirm917/Currency-Converter"
-              demo="https://currency-converter-one-blue.vercel.app"
-            />
-          </Box>
+        {carouselLayoutData.find(
+          (element) => element.tabIndex === tabIndex
+        ) && (
+          <CarouselLayout
+            element={carouselLayoutData.find(
+              (element) => element.tabIndex === tabIndex
+            )}
+          />
         )}
       </Box>
     </Box>
