a196a44a772eff006ce356823ea97f4add7df9f1
Shir Markowitz <shirm91799@gmail.com>
2024-01-05 00:03:53 -0500

    creating carouselLayoutData so we can condiontally render which carousel shows

---
+++ src/pages/Portfolio/data/carouselLayoutData.js
@@ -0,0 +1,40 @@
+import {
+  chatAppMedia,
+  weatherAppMedia,
+  memoryGameMedia,
+  recipeFinderMedia,
+  currencyConverterMedia,
+} from "./carouselData";
+
+export const carouselLayoutData = [
+  {
+    tabIndex: 0,
+    media: chatAppMedia,
+    github: "https://github.com/Shirm917/Final_Project",
+    demo: "https://final-project-yfoh.onrender.com",
+  },
+  {
+    tabIndex: 1,
+    media: weatherAppMedia,
+    github: "https://github.com/Shirm917/Weather-App",
+    demo: "https://weather-app-kohl-eta.vercel.app",
+  },
+  {
+    tabIndex: 2,
+    media: recipeFinderMedia,
+    github: "https://github.com/Shirm917/Recipe_Finder",
+    demo: "https://recipe-finder-kappa-two.vercel.app",
+  },
+  {
+    tabIndex: 3,
+    media: memoryGameMedia,
+    github: "https://github.com/Shirm917/Hackathon1",
+    demo: "https://memory-game-delta-vo.vercel.app",
+  },
+  {
+    tabIndex: 4,
+    media: currencyConverterMedia,
+    github: "https://github.com/Shirm917/Currency-Converter",
+    demo: "https://currency-converter-one-blue.vercel.app",
+  },
+];
