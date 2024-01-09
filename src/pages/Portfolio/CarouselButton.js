df01cea33ad7a402eb8878f3f68f197d5a714d7b
Shir Markowitz <shirm91799@gmail.com>
2024-01-08 23:45:05 -0500

    changing buttons to CarouselButton and making it one button to make it a resuable component

---
+++ src/pages/Portfolio/CarouselButton.js
@@ -0,0 +1,18 @@
+import Button from "@mui/material/Button";
+
+const CarouselButton = ({ link, title }) => {
+  return (
+    <Button
+      className="button"
+      variant="outlined"
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      sx={{ borderRadius: 5, margin: .5 }}
+    >
+      {title}
+    </Button>
+  );
+};
+
+export default CarouselButton;
